543fe73a5baafca04bd51f5d9213dfb91bd752fb
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-05-14 20:22:10 -0500

    first commit

---
+++ library-events-producer/src/main/java/com/library/domain/Book.java
@@ -0,0 +1,8 @@
+package com.library.domain;
+
+public record Book(
+        Integer bookId,
+        String bookName,
+        String bookAuthor
+) {
+}
---
+++ library-events-producer/src/main/java/com/library/domain/LibraryEvent.java
@@ -0,0 +1,8 @@
+package com.library.domain;
+
+public record LibraryEvent(
+        Integer libraryEventId,
+        LibraryEventType libraryEventType,
+        Book book
+) {
+}
